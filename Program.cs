ba271d5278dce3b8887eff5a752ff34d6cf20d38
Nicekloe <nice55kloe@gmail.com>
2023-05-04 21:20:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Ch04Ex02PrimeFactorsLib;
+
+
+Write("Enter a positive number between 1 and 1000: ");
+string? input = ReadLine();
+
+if (int.TryParse(input, out int result))
+{
+    WriteLine($"The prime factors of {result} are: {PrimeFactors.PrimeFactorsGenerate(result)}");
+}
+else
+{
+
+}
+
+
